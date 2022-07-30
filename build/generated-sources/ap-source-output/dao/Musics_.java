1c429ed93ede6b0f3414745f49a76631b91c546c
Carlos Bianchi Kremer <cbkremer@stefanini.com>
2022-07-30 19:26:52 -0300

    updated persistence.xml

---
+++ build/generated-sources/ap-source-output/dao/Musics_.java
@@ -0,0 +1,15 @@
+package dao;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-07-30T19:26:12")
+@StaticMetamodel(Musics.class)
+public class Musics_ { 
+
+    public static volatile SingularAttribute<Musics, String> artist;
+    public static volatile SingularAttribute<Musics, String> name;
+    public static volatile SingularAttribute<Musics, Long> id;
+
+}
